ec4342b163bef86fcfec243c155a9b4612ffeb10
wilsonz0 <wilsonzhang1313@gmail.com>
2026-03-14 17:22:17 -0400

    add ball movement

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    private float moveSpeed = 15f;
+    private Rigidbody rb;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        rb.useGravity = false;
+    }
+
+    void Update()
+    {
+        rb.linearVelocity = transform.forward * moveSpeed;
+        
+    }
+}
