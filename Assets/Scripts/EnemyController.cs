3da08013cec47397ee9d87ce166a745c058bc5fe
Bairu <bairuli1234@gmail.com>
2026-03-14 18:38:03 -0700

    add side bounce multiplier

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class EnemyController : PaddleBase
 {
     [SerializeField] private Transform ball;
-    private float enemyMoveSpeed = 2f;
+    public float enemyMoveSpeed = 4f;
 
     void Update()
     {
--- Assets/Scripts/PaddleBase.cs
+++ Assets/Scripts/PaddleBase.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public abstract class PaddleBase : MonoBehaviour
 {
+    public float sideMultiplier = 4f;
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
         {
             IBallBehavior ball = collision.gameObject.GetComponent<IBallBehavior>();
             Vector3 direction = CalculateBounceDirection(collision);
+            direction.x *= sideMultiplier;
+            direction.y *= sideMultiplier;
             ball.OnHitByPaddle(direction);
         }
     }
